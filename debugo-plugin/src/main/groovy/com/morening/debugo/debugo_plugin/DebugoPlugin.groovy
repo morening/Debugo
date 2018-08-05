d37f1ff893ee07a1975c8bafc3d1c1567efadc0b
morening <sunning_tj@163.com>
2018-08-05 20:53:35 +0800

    Change version to 0.0.6 for first alpha release

--- debugo-plugin/src/main/groovy/com/morening/debugo/debugo_plugin/DebugoPlugin.groovy
+++ debugo-plugin/src/main/groovy/com/morening/debugo/debugo_plugin/DebugoPlugin.groovy
@@ -27,9 +27,9 @@ class DebugoPlugin implements Plugin<Project> {
         }
 
         project.dependencies {
-            debugImplementation 'com.github.morening.Debugo:debugo-runtime:0.0.1'
+            debugImplementation 'com.github.morening.Debugo:debugo-runtime:0.0.6'
             debugImplementation 'org.aspectj:aspectjrt:1.8.6'
-            implementation 'com.github.morening.Debugo:debugo-annotations:0.0.1'
+            implementation 'com.github.morening.Debugo:debugo-annotations:0.0.6'
         }
 
         project.extensions.create('debugo', DebugoExtension)
