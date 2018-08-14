68b1479eb274bb92870def1a723e0e77757596b8
morening <sunning_tj@163.com>
2018-08-14 22:22:08 +0800

    Downgrade gradle plugin version to 2.3.0 and other responding 3rd libs' version to adapt projects well

--- debugo-plugin/src/main/groovy/com/morening/debugo/debugo_plugin/DebugoPlugin.groovy
+++ debugo-plugin/src/main/groovy/com/morening/debugo/debugo_plugin/DebugoPlugin.groovy
@@ -27,9 +27,9 @@ class DebugoPlugin implements Plugin<Project> {
         }
 
         project.dependencies {
-            debugImplementation 'com.github.morening.Debugo:debugo-runtime:0.0.27'
-            debugImplementation 'org.aspectj:aspectjrt:1.8.6'
-            implementation 'com.github.morening.Debugo:debugo-annotations:0.0.27'
+            compile 'com.github.morening.Debugo:debugo-runtime:0.0.27'
+            compile 'org.aspectj:aspectjrt:1.8.6'
+            compile 'com.github.morening.Debugo:debugo-annotations:0.0.27'
         }
 
         project.extensions.create('debugo', DebugoExtension)
